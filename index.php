36764204e5614b8bd427371489eabe3b8d8b5703
João Vitor <95136665+JoaoV357@users.noreply.github.com>
2022-01-23 00:16:39 -0300

    Aula 01

--- index.php
+++ index.php
@@ -4,11 +4,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Primeiro exemplo PHP</title>
+    <style>
+        body {
+            background-color: teal;
+        }
+        h2 {
+            color: aquamarine;
+            text-shadow: black 2px 2px 2px;
+        }
+        h1 {
+            text-shadow: white 2px 2px 2px;
+        }
+    </style>
 </head>
 <body>
+    <h1>Testando PHP</h1>
     <?php
-        echo "Olá, mundo!";
+        echo "<h2>Olá, <br> mundo!</h2>";
     ?>
 </body>
 </html>
